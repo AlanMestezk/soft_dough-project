d5f8362b9d24c4ea5f12643dbefc9ff49ff23a32
Alan Mestezk <95321834+AlanMestezk@users.noreply.github.com>
2023-03-15 09:34:49 -0300

    Section 2: Insert in tables

---
+++ Tabelas-DB/insert data in tables.sql
@@ -0,0 +1,28 @@
+INSERT INTO status(tipo)VALUES("Em produção");
+INSERT INTO status(tipo)VALUES("Partiu para entrega");
+INSERT INTO status(tipo)VALUES("Concluído");
+
+SELECT * FROM status;
+
+INSERT INTO massas(tipo)VALUES("Massa comum");
+INSERT INTO massas(tipo)VALUES("Massa temperada");
+INSERT INTO massas(tipo)VALUES("Massa integral");
+
+SELECT * FROM massas;
+
+INSERT INTO bordas(tipo)VALUES("Chedar");
+INSERT INTO bordas(tipo)VALUES("Catupiry");
+
+SELECT * FROM bordas;
+
+INSERT INTO sabores(nome)VALUES("4 queijos");
+INSERT INTO sabores(nome)VALUES("Frango com catupiry");
+INSERT INTO sabores(nome)VALUES("Calabresa");
+INSERT INTO sabores(nome)VALUES("Lombinho");
+INSERT INTO sabores(nome)VALUES("Filé com chedar");
+INSERT INTO sabores(nome)VALUES("Portuguesa");
+INSERT INTO sabores(nome)VALUES("Marguerita");
+
+SELECT * FROM sabores;
+
+
